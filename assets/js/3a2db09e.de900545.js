fe670f76cb0361b0e2b2b3cb8be5afcf03542b6f
caioricciuti <caio.ricciuti@outlook.com>
2025-02-01 19:05:17 +0000

    deploy: 120a6689f078da9a97475f929ef7a5b241227657

---
+++ assets/js/3a2db09e.de900545.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[121],{8070:e=>{e.exports=JSON.parse('{"tags":[{"label":"Duck-UI","permalink":"/blog/tags/duck-ui","description":"More on the UI topic!","count":1}]}')}}]);
